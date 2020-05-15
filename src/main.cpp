837fd2372435e012d96ee53b89a51ab58595702a
Alexej Harm <alexej.h@xiphos.de>
2020-05-15 18:20:14 +0200

    modernized scripts and code

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,33 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <cstdio>
+#include <cstdlib>
 
-#include <stdio.h>
+int main(int argc, char* args[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+    printf("SDL init failed.");
+    return EXIT_FAILURE;
+  }
+  if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+    printf("SDL image failed to initialize PNG subsystem.");
+    return EXIT_FAILURE;
+  }
 
-int main(int argc, char* args[])
-{
-	if (SDL_Init(SDL_INIT_VIDEO) > 0) {
-		printf("SDL init failed.");
-		return 1;
-	}
-	if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
-		printf("SDL image failed to initialize PNG subsystem.");
-		return 1;
-	}
+  auto window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+  auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Window* gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-    SDL_Renderer* gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-    
-    SDL_Surface* loadedSurface = IMG_Load("data/img.png");
+  auto loaded_surface = IMG_Load("data/img.png");
+  auto texture = SDL_CreateTextureFromSurface(renderer, loaded_surface);
+  SDL_FreeSurface(loaded_surface);
 
-    SDL_Texture* newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-        
-    SDL_FreeSurface(loadedSurface);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+  SDL_RenderPresent(renderer);
 
-    SDL_RenderClear(gRenderer);
-    SDL_RenderCopy(gRenderer, newTexture, NULL, NULL);
-    SDL_RenderPresent(gRenderer);
+  SDL_Delay(2000);
 
-    SDL_Delay(2000);
-    return 0;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+
+  return EXIT_SUCCESS;
 }
